751800bdd4ebdb1184c649ceba866e5800f50bfd
Joseph Diaz <194326262+josephhdiazg@users.noreply.github.com>
2026-04-21 18:14:29 -0500

    build: add spring starter template

---
+++ src/main/java/co/edu/ustavillavo/desemp/taller_bookstore/TallerBookstoreApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ustavillavo.desemp.taller_bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/ustavillavo/desemp/taller_bookstore/TallerBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ustavillavo.desemp.taller_bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
