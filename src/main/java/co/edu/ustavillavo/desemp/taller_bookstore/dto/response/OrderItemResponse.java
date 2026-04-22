d69ab06e295a467ce23ac50c4fb71b7f14701d87
michael <michaelarias20115@gmail.com>
2026-04-22 11:41:33 -0500

    feat: add request and response DTOs for all entities

---
+++ src/main/java/co/edu/ustavillavo/desemp/taller_bookstore/dto/response/OrderItemResponse.java
@@ -0,0 +1,15 @@
+package co.edu.ustavillavo.desemp.taller_bookstore.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import java.math.BigDecimal;
+
+@Getter
+@Builder
+public class OrderItemResponse {
+    private Long id;
+    private Long bookId;
+    private String bookTitle;
+    private Integer quantity;
+    private BigDecimal subtotal;
+}
