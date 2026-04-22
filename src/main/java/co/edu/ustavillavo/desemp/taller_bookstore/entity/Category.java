e6c5bb60ae01613fb35051708df51b3ea39bb58e
michael <michaelarias20115@gmail.com>
2026-04-21 23:11:21 -0500

    feat: add domain entities (Author, Category, Book, Order, OrderItem)

---
+++ src/main/java/co/edu/ustavillavo/desemp/taller_bookstore/entity/Category.java
@@ -0,0 +1,28 @@
+package co.edu.ustavillavo.desemp.taller_bookstore.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories", fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<Book> books = new ArrayList<>();
+}
---
+++ src/main/java/co/edu/ustavillavo/desemp/taller_bookstore/entity/OrderStatus.java
@@ -0,0 +1,7 @@
+package co.edu.ustavillavo.desemp.taller_bookstore.entity;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    CANCELLED
+}
